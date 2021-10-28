4bac03b7e69cd912fb3f2e792820abb7f7ce85c0
Cédric Anne <cedric.anne@gmail.com>
2021-10-28 08:43:16 +0200

    Fix headers

--- ajax/taskjobdeletetype.php
+++ ajax/taskjobdeletetype.php
@@ -1,33 +1,33 @@
 <?php
 /**
- *  * ---------------------------------------------------------------------
- *  * GLPI Inventory Plugin
- *  * Copyright (C) 2021 Teclib' and contributors.
- *  *
- *  * http://glpi-project.org
- *  *
- *  * based on FusionInventory for GLPI
- *  * Copyright (C) 2010-2021 by the FusionInventory Development Team.
- *  *
- *  * ---------------------------------------------------------------------
- *  *
- *  * LICENSE
- *  *
- *  * This file is part of GLPI Inventory Plugin.
- *  *
- *  * GLPI Inventory Plugin is free software: you can redistribute it and/or modify
- *  * it under the terms of the GNU Affero General Public License as published by
- *  * the Free Software Foundation, either version 3 of the License, or
- *  * (at your option) any later version.
- *  *
- *  * GLPI Inventoruy Plugin is distributed in the hope that it will be useful,
- *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  * GNU Affero General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU Affero General Public License
- *  * along with GLPI Inventory Plugin. If not, see <https://www.gnu.org/licenses/>.
- *  * ---------------------------------------------------------------------
+ * ---------------------------------------------------------------------
+ * GLPI Inventory Plugin
+ * Copyright (C) 2021 Teclib' and contributors.
+ *
+ * http://glpi-project.org
+ *
+ * based on FusionInventory for GLPI
+ * Copyright (C) 2010-2021 by the FusionInventory Development Team.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI Inventory Plugin.
+ *
+ * GLPI Inventory Plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GLPI Inventoruy Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with GLPI Inventory Plugin. If not, see <https://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
  */
 
 if (strpos(filter_input(INPUT_SERVER, "PHP_SELF"), "taskjobdeletetype.php")) {
