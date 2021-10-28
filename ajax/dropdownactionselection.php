01960210a05e26bce37006ac0da64f2133d365da
Johan Cwiklinski <johan@x-tnd.be>
2021-10-28 08:20:03 +0200

    Fix headers

--- ajax/dropdownactionselection.php
+++ ajax/dropdownactionselection.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * ---------------------------------------------------------------------
- * GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2015-2021 Teclib' and contributors.
- *
- * http://glpi-project.org
- *
- * based on GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2003-2014 by the INDEPNET Development Team.
- *
- * ---------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of GLPI.
- *
- * GLPI is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * GLPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
- * ---------------------------------------------------------------------
+ *  * ---------------------------------------------------------------------
+ *  * GLPI Inventory Plugin
+ *  * Copyright (C) 2021 Teclib' and contributors.
+ *  *
+ *  * http://glpi-project.org
+ *  *
+ *  * based on FusionInventory for GLPI
+ *  * Copyright (C) 2010-2021 by the FusionInventory Development Team.
+ *  *
+ *  * ---------------------------------------------------------------------
+ *  *
+ *  * LICENSE
+ *  *
+ *  * This file is part of GLPI Inventory Plugin.
+ *  *
+ *  * GLPI Inventory Plugin is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * GLPI Inventoruy Plugin is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU Affero General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  * along with GLPI Inventory Plugin. If not, see <https://www.gnu.org/licenses/>.
+ *  * ---------------------------------------------------------------------
  */
 
 if (strpos(filter_input(INPUT_SERVER, "PHP_SELF"), "dropdownactionselection.php")) {
