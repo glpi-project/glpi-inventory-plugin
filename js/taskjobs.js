77f25a6f9796ee4923bf27b0f1cdfda8a190f9ad
Curtis Conard <cconard96@gmail.com>
2022-03-05 07:43:42 -0500

    Fix typo in license headers

--- js/taskjobs.js
+++ js/taskjobs.js
@@ -19,7 +19,7 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * GLPI Inventoruy Plugin is distributed in the hope that it will be useful,
+ * GLPI Inventory Plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
