8a3d2c9cea77012faf76081d07b938b17de026b7
stonebuzz <skita@teclib.com>
2022-11-25 14:39:06 +0100

    fix(menu): display menu on update

--- front/credentialip.form.php
+++ front/credentialip.form.php
@@ -31,10 +31,85 @@
  * ---------------------------------------------------------------------
  */
 
-include("../../../inc/includes.php");
+include('../../../inc/includes.php');
+$dropdown = new PluginGlpiinventoryCredentialIP();
 
-$dropdown = new PluginGlpiinventoryCredentialIp();
-if (empty($_POST['plugin_glpiinventory_credentials_id'])) {
-    $_POST['plugin_glpiinventory_credentials_id'] = 0;
+if (!($dropdown instanceof CommonDropdown)) {
+    Html::displayErrorAndDie('');
 }
-include(GLPI_ROOT . "/front/dropdown.common.form.php");
+if (!$dropdown->canView()) {
+   // Gestion timeout session
+    Session::redirectIfNotLoggedIn();
+    Html::displayRightError();
+}
+
+if (isset($_POST["id"])) {
+    $_GET["id"] = $_POST["id"];
+} else if (!isset($_GET["id"])) {
+    $_GET["id"] = -1;
+}
+
+if (isset($_POST["add"])) {
+    $dropdown->check(-1, CREATE, $_POST);
+
+    if ($newID = $dropdown->add($_POST)) {
+        if ($_SESSION['glpibackcreated']) {
+            $url = $dropdown->getLinkURL();
+            if (isset($_REQUEST['_in_modal'])) {
+                $url .= "&_in_modal=1";
+            }
+            Html::redirect($url);
+        }
+    }
+    Html::back();
+} else if (isset($_POST["purge"])) {
+    $dropdown->check($_POST["id"], PURGE);
+    if (
+        $dropdown->isUsed()
+        && empty($_POST["forcepurge"])
+    ) {
+        Html::header(
+            $dropdown->getTypeName(1),
+            $_SERVER['PHP_SELF'],
+            "config",
+            $dropdown->second_level_menu,
+            str_replace('glpi_', '', $dropdown->getTable())
+        );
+        $dropdown->showDeleteConfirmForm($_SERVER['PHP_SELF']);
+        Html::footer();
+    } else {
+        $dropdown->delete($_POST, 1);
+        $dropdown->redirectToList();
+    }
+} else if (isset($_POST["replace"])) {
+    $dropdown->check($_POST["id"], PURGE);
+    $dropdown->delete($_POST, 1);
+    $dropdown->redirectToList();
+} else if (isset($_POST["update"])) {
+    $dropdown->check($_POST["id"], UPDATE);
+    $dropdown->update($_POST);
+    Html::back();
+} else if (isset($_GET['_in_modal'])) {
+    Html::popHeader(
+        $dropdown->getTypeName(1),
+        $_SERVER['PHP_SELF'],
+        true,
+        $dropdown->first_level_menu,
+        $dropdown->second_level_menu,
+        $dropdown->getType()
+    );
+    $dropdown->showForm($_GET["id"]);
+    Html::popFooter();
+} else {
+    Html::header(
+        __('GLPI Inventory', 'glpiinventory'),
+        $_SERVER["PHP_SELF"],
+        "admin",
+        "pluginglpiinventorymenu",
+        "credential"
+    );
+    PluginGlpiinventoryMenu::displayMenu("mini");
+    //If there is no form to submit, display the form
+    $dropdown->display($_GET);
+    Html::footer();
+}
