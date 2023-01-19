710aa2699856867726231e4a7adf0798de52aeae
Stanislas <skita@teclib.com>
2023-01-19 14:00:14 +0100

    remove useless global
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- front/deployfile_download.php
+++ front/deployfile_download.php
@@ -34,7 +34,6 @@
 include("../../../inc/includes.php");
 
 Session::checkRight('plugin_glpiinventory_package', READ);
-global $CFG_GLPI;
 
 session_write_close(); // unlock session to ensure GLPI is still usable while huge file downloads is done in background
 
