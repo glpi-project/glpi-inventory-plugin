5042658215d3266494a638f225e26985f2b26dcc
Johan Cwiklinski <johan@x-tnd.be>
2023-04-11 08:46:48 +0200

    Use REQUEST_URI rather than SCRIPT_NAME; fixes #342

--- setup.php
+++ setup.php
@@ -69,7 +69,8 @@ function plugin_glpiinventory_script_endswith($scriptname)
 {
     //append plugin directory to avoid dumb errors...
     $scriptname = 'glpiinventory/front/' . $scriptname;
-    $script_name = filter_input(INPUT_SERVER, "SCRIPT_NAME");
+    $script_name = filter_input(INPUT_SERVER, "REQUEST_URI");
+
     return substr($script_name, -strlen($scriptname)) === $scriptname;
 }
 
@@ -234,9 +235,9 @@ function plugin_init_glpiinventory()
         $PLUGIN_HOOKS['add_javascript']['glpiinventory'] = [];
         $PLUGIN_HOOKS['add_css']['glpiinventory'] = [];
         if (
-            strpos(filter_input(INPUT_SERVER, "SCRIPT_NAME"), Plugin::getWebDir('glpiinventory', false)) != false
-            || strpos(filter_input(INPUT_SERVER, "SCRIPT_NAME"), "front/printer.form.php") != false
-            || strpos(filter_input(INPUT_SERVER, "SCRIPT_NAME"), "front/computer.form.php") != false
+            strpos(filter_input(INPUT_SERVER, "REQUEST_URI"), Plugin::getWebDir('glpiinventory', false)) != false
+            || strpos(filter_input(INPUT_SERVER, "REQUEST_URI"), "front/printer.form.php") != false
+            || strpos(filter_input(INPUT_SERVER, "REQUEST_URI"), "front/computer.form.php") != false
         ) {
             $PLUGIN_HOOKS['add_css']['glpiinventory'][] = "css/views.css";
             $PLUGIN_HOOKS['add_css']['glpiinventory'][] = "css/deploy.css";
@@ -324,7 +325,7 @@ function plugin_init_glpiinventory()
         }
 
        // load task view css for computer self deploy (tech)
-        if (strpos(filter_input(INPUT_SERVER, "SCRIPT_NAME"), "front/computer.form.php") != false) {
+        if (strpos(filter_input(INPUT_SERVER, "REQUEST_URI"), "front/computer.form.php") != false) {
             $PLUGIN_HOOKS['add_css']['glpiinventory'][] = "css/views.css";
         }
 
