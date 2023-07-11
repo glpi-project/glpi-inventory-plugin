a7a170952812a6a8bec04964788d39bfaf99bab3
Cédric Anne <cedric.anne@gmail.com>
2023-07-11 15:06:03 +0200

    1.2.3

--- setup.php
+++ setup.php
@@ -33,7 +33,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define("PLUGIN_GLPIINVENTORY_VERSION", "1.2.2");
+define("PLUGIN_GLPIINVENTORY_VERSION", "1.2.3");
 // Minimal GLPI version, inclusive
 define('PLUGIN_GLPI_INVENTORY_GLPI_MIN_VERSION', '10.0.8');
 // Maximum GLPI version, exclusive
