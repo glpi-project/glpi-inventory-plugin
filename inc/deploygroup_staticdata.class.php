1c4d4e0ba81b46207733df3f84308dbdcbf25ef4
stonebuzz <skita@teclib.com>
2023-11-10 14:26:36 +0100

    fix(StaticGroup): fix SQL criteria

--- inc/deploygroup_staticdata.class.php
+++ inc/deploygroup_staticdata.class.php
@@ -170,12 +170,12 @@ class PluginGlpiinventoryDeployGroup_Staticdata extends CommonDBRelation
         $search_params['massiveactionparams']['extraparams']['specific_actions']['PluginGlpiinventoryComputer' . MassiveAction::CLASS_ACTION_SEPARATOR . 'add'] = __('Add to static group', 'glpiinventory');
         $search_params['massiveactionparams']['extraparams']['massive_action_fields'] = ['action', 'id'];
 
-        $data = Search::prepareDatasForSearch('PluginGlpiinventoryComputer', $search_params);
+        $data = Search::prepareDatasForSearch('Computer', $search_params);
         $data['itemtype'] = 'Computer';
         Search::constructSQL($data);
         Search::constructData($data);
         $data['search']['target'] = PluginGlpiinventoryDeployGroup::getSearchEngineTargetURL($item->getID(), false);
-        $data['itemtype'] = 'PluginGlpiinventoryComputer';
+        $data['itemtype'] = 'Computer';
         $limit_backup = $_SESSION['glpilist_limit'];
         $_SESSION['glpilist_limit'] = 200;
         Search::displayData($data);
