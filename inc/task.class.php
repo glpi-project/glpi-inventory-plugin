3ce9cb4c718f1f42324b74bb9c50fa55d85140cf
Romain B <8530352+Rom1-B@users.noreply.github.com>
2024-04-15 09:08:53 +0200

    fix: cancelled count (#499)
    * fix: cancelled count
    
    * same issue with not done yet
    
    * fix test

--- inc/task.class.php
+++ inc/task.class.php
@@ -1124,6 +1124,9 @@ class PluginGlpiinventoryTask extends PluginGlpiinventoryTaskView
                         if (
                             !isset($counters['agents_prepared'][$agent_id])
                             && !isset($counters['agents_running'][$agent_id])
+                            && !isset($counters['agents_success'][$agent_id])
+                            && !isset($counters['agents_error'][$agent_id])
+                            && !isset($counters['agents_notdone'][$agent_id])
                         ) {
                             $counters['agents_cancelled'][$agent_id] = $run_id;
                             $agent_state = 'cancelled';
@@ -1210,6 +1213,7 @@ class PluginGlpiinventoryTask extends PluginGlpiinventoryTaskView
                 if (
                     !isset($counters['agents_error'][$agent_id])
                     && !isset($counters['agents_success'][$agent_id])
+                    && !isset($counters['agents_cancelled'][$agent_id])
                 ) {
                     $counters['agents_notdone'][$agent_id] = $run_id;
                 }
