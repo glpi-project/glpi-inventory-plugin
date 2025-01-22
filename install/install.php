501af7cf68d0f13ed613fd3c03bdc1bc23b4febf
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-01-22 18:21:19 -0300

    Remove translation of crontask comments
    GLPI is multilanguage so translating the comment it uses the language of the user when the plug-in has been installed.

--- install/install.php
+++ install/install.php
@@ -262,17 +262,16 @@ function pluginGlpiinventoryInstall($version, $migrationname = 'Migration')
         'wakeupAgents',
         120,
         ['mode' => 2, 'allowmode' => 3, 'logs_lifetime' => 30,
-        'comment' => Toolbox::addslashes_deep(__(
-            'Wake agents ups',
-            'glpiinventory'
-        ))]
+        'comment' => Toolbox::addslashes_deep(
+            'Wake agents ups'
+        )]
     );
     CronTask::Register(
         'PluginGlpiinventoryTask',
         'cleanondemand',
         86400,
         ['mode' => 2, 'allowmode' => 3, 'logs_lifetime' => 30,
-        'comment' => Toolbox::addslashes_deep(__('Clean on demand deployment tasks'))]
+        'comment' => Toolbox::addslashes_deep('Clean on demand deployment tasks')]
     );
 
    /*
