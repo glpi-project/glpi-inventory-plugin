713de9ef01454ceb1d277611796c239aec41fed1
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-01-27 20:18:06 -0300

    Fix concatenation

--- inc/taskjoblog.class.php
+++ inc/taskjoblog.class.php
@@ -356,7 +356,7 @@ class PluginGlpiinventoryTaskjoblog extends CommonDBTM
             if ($classname != '' && class_exists($classname)) {
                 $Class = new $classname();
                 $Class->getFromDB($matches[2][$num]);
-                $comment = $Class->getTypeName() + " " + str_replace($commentvalue, $Class->getLink(), $comment);
+                $comment = $Class->getTypeName() . " " . str_replace($commentvalue, $Class->getLink(), $comment);
             }
         }
         if (strstr($comment, "==")) {
