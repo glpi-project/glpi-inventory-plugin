4a3f21553f76b62ffada9f2c062d604f8ed8aa39
Johan Cwiklinski <johan@x-tnd.be>
2025-03-26 14:03:26 +0100

    Fix CS

--- inc/deploytaskjob.class.php
+++ inc/deploytaskjob.class.php
@@ -35,7 +35,7 @@ if (!defined('GLPI_ROOT')) {
     die("Sorry. You can't access directly to this file");
 }
 
-use \Glpi\DBAL\QueryParam;
+use Glpi\DBAL\QueryParam;
 
 /**
  * Manage the deploy task job.
