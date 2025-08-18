a51536f2f7aa47341178a2c5f69654b31ce526e8
Johan Cwiklinski <johan@x-tnd.be>
2025-08-18 14:43:47 +0200

    Cleanup useless files

--- public/lib/lazy.js-0.5.1/site/source/javascripts/comparisons_all.js
+++
@@ -1,3 +0,0 @@
-//= require common
-//= require boot
-//= require comparisons
--- public/lib/lazy.js-0.5.1/spec/amd_spec.js
+++
@@ -1,5 +0,0 @@
-var path = require('path'),
-    requirejs = require('requirejs'),
-    test_path = path.join(__dirname, 'amd_spec_test.js');
-
-requirejs([test_path]);
