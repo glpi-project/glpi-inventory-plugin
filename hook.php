a24b26a32aa6405976b6c137b0c0ede45e5d07f0
Stanislas Kita <skita@teclib.com>
2025-08-21 16:48:49 +0200

    test

--- hook.php
+++ hook.php
@@ -36,10 +36,10 @@ function plugin_glpiinventory_add_default_where($in)
     [$itemtype, $condition] = $in;
 
     if ($itemtype === 'Computer') {
-        $requestUri = $_SERVER['REQUEST_URI'] ?? '';
-        if (str_contains($requestUri, 'deploypackage.public.php')) {
-            $condition = [];
-        }
+        //$requestUri = $_SERVER['REQUEST_URI'] ?? '';
+        //if (str_contains($requestUri, 'deploypackage.public.php')) {
+        $condition = [];
+        //}
     }
 
     return [$itemtype, $condition];
