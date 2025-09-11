56fafe7869c7262a562d4e8482a6ef837efd3ea2
Johan Cwiklinski <johan@x-tnd.be>
2025-09-11 14:17:18 +0200

    Move logic in class

--- front/collect_file.form.php
+++ front/collect_file.form.php
@@ -35,38 +35,6 @@
 $pfCollect_File = new PluginGlpiinventoryCollect_File();
 
 if (isset($_POST["add"])) {
-    // conversions
-    if (
-        $_POST['sizetype'] != 'none'
-           && $_POST['size'] != ''
-    ) {
-        $_POST['filter_size' . $_POST['sizetype']] = $_POST['size'];
-    }
-    if (
-        $_POST['filter_nametype'] != 'none'
-           && $_POST['filter_name'] != ''
-    ) {
-        $_POST['filter_' . $_POST['filter_nametype']] = $_POST['filter_name'];
-
-        //set null if needed
-        if ($_POST['filter_nametype'] == 'iname') {
-            $_POST['filter_name'] = null;
-        } else {
-            $_POST['filter_iname'] = null;
-        }
-    } else {
-        //if 'none' , name and iname need to be null
-        $_POST['filter_iname'] = null;
-        $_POST['filter_name'] = null;
-    }
-    if ($_POST['type'] == 'file') {
-        $_POST['filter_is_file'] = 1;
-        $_POST['filter_is_dir'] = 0;
-    } else {
-        $_POST['filter_is_file'] = 0;
-        $_POST['filter_is_dir'] = 1;
-    }
-
     $pfCollect_File->add($_POST);
     Html::back();
 } elseif (isset($_POST["delete"])) {
