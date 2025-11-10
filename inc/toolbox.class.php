8fd47a9e532b766a5afc138101ef703ca0dcdb06
Stanislas <skita@teclib.com>
2025-11-10 14:55:25 +0100

    Fix(Core): Agent request handle as system user (#800)
    * Fix(Core): Agent request handle as system user
    
    * fix CS
    
    * force interface
    
    * simplify

--- inc/toolbox.class.php
+++ inc/toolbox.class.php
@@ -352,6 +352,8 @@ class PluginGlpiinventoryToolbox
          = "'" . implode("', '", $_SESSION['glpiactiveentities']) . "'";
         $_SESSION['glpiparententities'] = [];
 
+        $_SESSION['glpiactiveprofile']['interface'] = 'central';
+
         // Execute function with impersonated SESSION
         $result = call_user_func_array($function, $args);
 
