519b37f532a4e7a00bd295278b8809f060e2ea02
Johan Cwiklinski <johan@x-tnd.be>
2026-01-06 16:02:15 +0100

    Merge conditions, drop duplicated header

--- hook.php
+++ hook.php
@@ -38,47 +38,16 @@ use function Safe\ini_set;
 use function Safe\json_decode;
 
 /**
- * ---------------------------------------------------------------------
- * GLPI Inventory Plugin
- * Copyright (C) 2021 Teclib' and contributors.
- *
- * http://glpi-project.org
- *
- * based on FusionInventory for GLPI
- * Copyright (C) 2010-2021 by the FusionInventory Development Team.
- *
- * ---------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of GLPI Inventory Plugin.
- *
- * GLPI Inventory Plugin is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GLPI Inventory Plugin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with GLPI Inventory Plugin. If not, see <https://www.gnu.org/licenses/>.
- * ---------------------------------------------------------------------
- */
-
-/**
  * Add search options for GLPI objects
  *
- * @param string $itemtype
+ * @param class-string<CommonDBTM> $itemtype
  * @return array
  */
 function plugin_glpiinventory_getAddSearchOptions($itemtype)
 {
 
     $sopt = [];
-    if ($itemtype == 'Computer') {
+    if ($itemtype === Computer::class) {
         $sopt[5164]['table']         = "glpi_plugin_glpiinventory_agentmodules";
         $sopt[5164]['field']         = "DEPLOY";
         $sopt[5164]['linkfield']     = "DEPLOY";
@@ -135,9 +104,7 @@ function plugin_glpiinventory_getAddSearchOptions($itemtype)
                                            ],
                                        ],
         ];
-    }
 
-    if ($itemtype == Computer::class) {
         $sopt += PluginGlpiinventoryCollect::getSearchOptionsToAdd();
     }
 
