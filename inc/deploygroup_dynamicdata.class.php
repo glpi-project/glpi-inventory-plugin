d6bd2d0465b7da2091ee9e7f7fcedb6dc47367e7
Rom1-B <8530352+Rom1-B@users.noreply.github.com>
2026-07-10 10:18:15 +0200

    fix double header

--- inc/deploygroup_dynamicdata.class.php
+++ inc/deploygroup_dynamicdata.class.php
@@ -32,37 +32,6 @@
 
 use Safe\Exceptions\JsonException;
 
-/**
- * ---------------------------------------------------------------------
- * GLPI Inventory Plugin
- * Copyright (C) 2021 Teclib' and contributors.
- *
- * http://glpi-project.org
- *
- * @basedon   FusionInventory for GLPI
- * @copyright 2010-2021 by the FusionInventory Development Team.
- *
- * ---------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of GLPI Inventory Plugin.
- *
- * GLPI Inventory Plugin is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GLPI Inventory Plugin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with GLPI Inventory Plugin. If not, see <https://www.gnu.org/licenses/>.
- * ---------------------------------------------------------------------
- */
-
 use function Safe\json_decode;
 use function Safe\json_encode;
 
