50682ed1c7cb978f9940c137a151e96b0ad23b95
Stanislas <skita@teclib.com>
2026-09-21 15:22:54 +0200

    Update inc/collect_registry_content.class.php
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>

--- inc/collect_registry_content.class.php
+++ inc/collect_registry_content.class.php
@@ -211,7 +211,8 @@ class PluginGlpiinventoryCollect_Registry_Content extends PluginGlpiinventoryCol
         echo "<tr>";
         echo "<th colspan='3'>";
         echo $pfCollect_Registry->fields['hive'] .
-           $pfCollect_Registry->fields['path'];
+echo htmlspecialchars($pfCollect_Registry->fields['hive'] ?? '' .
+$pfCollect_Registry->fields['path'] ?? '');
         echo "</th>";
         echo "</tr>";
 
